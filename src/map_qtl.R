aac8833ab3dac280c4df921818b1f1e2f16bc292
basel-maher <bma8ne@virginia.edu>
2019-06-06 13:10:26 -0400

    dont map weight, dont log transform age, use the proper kinship input, etc

--- src/map_qtl.R
+++ src/map_qtl.R
@@ -8,7 +8,7 @@ load(file = "./results/Rdata/cross_basic_cleaned.Rdata")
 
 #load kinship file. In this case, using LOCO but can use overall file too
 #load(file = "./results/Rdata/k_basic_cleaned.Rdata") #overall (not used)
-load(file = "./results/Rdata/k_loco_basic_cleaned_pseudo.Rdata") #LOCO
+load(file = "./results/Rdata/k_loco_basic_cleaned.Rdata") #LOCO
 
 #get Xcovar
 Xcovar <- get_x_covar(cross_basic)
@@ -34,14 +34,13 @@ for(i in quant_pheno_columns){
 
 
 #qtl mapping for continuous traits
-#paste0(colnames(norm_pheno),"_LOGTRANS")
 
 #which traits require FL as covar? bending traits?
-DO_qtl_scan = scan1(apr, cross_basic$pheno[,c(5:70,72,74,76)], k_loco, Xcovar=Xcovar, addcovar = covar[,c("sex", "age_at_sac_days","body_weight","generationG24","generationG25","generationG26","generationG27","generationG28","generationG29","generationG30","generationG31","generationG32","generationG33")],cores = 0)
+DO_qtl_scan = scan1(apr, cross_basic$pheno[,c(6:70,72,74,76)], k_loco, Xcovar=Xcovar, addcovar = covar[,c("sex", "age_at_sac_days","body_weight","generationG24","generationG25","generationG26","generationG27","generationG28","generationG29","generationG30","generationG31","generationG32","generationG33")],cores = 2)
 save(DO_qtl_scan,file = "./results/Rdata/DO_qtl_scan.Rdata")
 
 
-DO_qtl_scan_binary = scan1(apr, cross_basic$pheno[,c(71,73,75,77)], Xcovar=Xcovar, addcovar = covar[,c("sex", "age_at_sac_days","body_weight","generationG24","generationG25","generationG26","generationG27","generationG28","generationG29","generationG30","generationG31","generationG32","generationG33")],cores = 0)
+DO_qtl_scan_binary = scan1(apr, cross_basic$pheno[,c(71,73,75,77)], Xcovar=Xcovar, addcovar = covar[,c("sex", "age_at_sac_days","body_weight","generationG24","generationG25","generationG26","generationG27","generationG28","generationG29","generationG30","generationG31","generationG32","generationG33")],cores = 2)
 save(DO_qtl_scan_binary,file = "./results/Rdata/DO_qtl_scan_binary.Rdata")
 
 #find peaks and bind them together
@@ -53,14 +52,14 @@ write.csv(qtl_peaks_both, file = "qtl_peaks.csv",row.names = FALSE,quote = FALSE
 
 
 ####try while Normal####
-norm_pheno = as.data.frame(log10(cross_basic$pheno[,c(5:14,16,17,21,23:32,34,35,37:41,43:49,51,52,54:58,60:70,72,74,76)]+0.0000001))
+norm_pheno = as.data.frame(log10(cross_basic$pheno[,c(6:14,16,17,21,23:32,34,35,37:41,43:49,51,52,54:58,60:70,72,74,76)]+0.0000001))
 pheno_combined = cbind(norm_pheno, cross_basic$pheno[,c(15,18,19,20,22,33,36,42,50,53,59)])
 new_covar = covar
-new_covar[,c(2,3)] = log10(new_covar[,c(2,3)]+0.0000001)
-DO_qtl_scan_normal = scan1(apr, pheno_combined, k_loco, Xcovar=Xcovar, addcovar = new_covar[,c("sex", "age_at_sac_days","body_weight","generationG24","generationG25","generationG26","generationG27","generationG28","generationG29","generationG30","generationG31","generationG32","generationG33")],cores = 0)
+new_covar[,c(3)] = log10(new_covar[,c(3)]+0.0000001)
+DO_qtl_scan_normal = scan1(apr, pheno_combined, k_loco, Xcovar=Xcovar, addcovar = new_covar[,c("sex", "age_at_sac_days","body_weight","generationG24","generationG25","generationG26","generationG27","generationG28","generationG29","generationG30","generationG31","generationG32","generationG33")],cores = 2)
 save(DO_qtl_scan_normal,file = "./results/Rdata/DO_qtl_scan_norm.Rdata")
 
-DO_qtl_scan_binary_norm = scan1(apr, cross_basic$pheno[,c(71,73,75,77)], Xcovar=Xcovar, addcovar = new_covar[,c("sex", "age_at_sac_days","body_weight","generationG24","generationG25","generationG26","generationG27","generationG28","generationG29","generationG30","generationG31","generationG32","generationG33")])
+DO_qtl_scan_binary_norm = scan1(apr, cross_basic$pheno[,c(71,73,75,77)], Xcovar=Xcovar, addcovar = new_covar[,c("sex", "age_at_sac_days","body_weight","generationG24","generationG25","generationG26","generationG27","generationG28","generationG29","generationG30","generationG31","generationG32","generationG33")],cores = 2)
 
 qtl_peaks_bin_norm = find_peaks(DO_qtl_scan_binary_norm, cross_basic$pmap, threshold=4, drop=1.5)
 qtl_peaks_both_norm = rbind(qtl_peaks_norm,qtl_peaks_bin_norm)
@@ -69,5 +68,5 @@ qtl_peaks_both_norm = rbind(qtl_peaks_norm,qtl_peaks_bin_norm)
 norm_perm = scan1perm(apr,pheno_combined,k_loco, Xcovar=Xcovar, addcovar = new_covar[,c("sex", "age_at_sac_days","body_weight","generationG24","generationG25","generationG26","generationG27","generationG28","generationG29","generationG30","generationG31","generationG32","generationG33")],n_perm = 1000,perm_Xsp = TRUE,cores = 0)
 save(norm_perm,file = "./results/Rdata/norm_perm.Rdata")
 
-perm = scan1perm(apr, cross_basic$pheno[,c(5:70,72,74,76)], k_loco, Xcovar=Xcovar, addcovar = covar[,c("sex", "age_at_sac_days","body_weight","generationG24","generationG25","generationG26","generationG27","generationG28","generationG29","generationG30","generationG31","generationG32","generationG33")],n_perm = 1000,perm_Xsp = TRUE, cores=0)
+perm = scan1perm(apr, cross_basic$pheno[,c(6:70,72,74,76)], k_loco, Xcovar=Xcovar, addcovar = covar[,c("sex", "age_at_sac_days","body_weight","generationG24","generationG25","generationG26","generationG27","generationG28","generationG29","generationG30","generationG31","generationG32","generationG33")],n_perm = 1000,perm_Xsp = TRUE, cores=0)
 save(perm,file = "./results/Rdata/perm.Rdata")
