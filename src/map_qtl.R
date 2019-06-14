44b00fdb8e8c944c76c018616f282acd3d9f53b3
basel-maher <bma8ne@virginia.edu>
2019-06-14 14:42:19 -0400

    added a bunch of stuff, not ready for publishing. will remove comments

--- src/map_qtl.R
+++ src/map_qtl.R
@@ -1,15 +1,15 @@
 #QTL mapping 
 library(qtl2)
-
+load(file = "./results/Rdata/pr_basic_cleaned.Rdata")
 #load the allele probs
-load(file = "./results/Rdata/apr_basic_cleaned_pseudo.Rdata")
+load(file = "./results/Rdata/apr_basic_cleaned.Rdata")
 #load the cross file 
 load(file = "./results/Rdata/cross_basic_cleaned.Rdata")
 
 #load kinship file. In this case, using LOCO but can use overall file too
 #load(file = "./results/Rdata/k_basic_cleaned.Rdata") #overall (not used)
 load(file = "./results/Rdata/k_loco_basic_cleaned.Rdata") #LOCO
-
+load(file = "./results/Rdata/k_basic_cleaned.Rdata")
 #get Xcovar
 Xcovar <- get_x_covar(cross_basic)
 
@@ -21,6 +21,9 @@ covar = covar[,-1]#remove sac date as covar for now
 
 covar = apply(covar,2,as.numeric) #make sure all cols are numeric
 rownames(covar) = rownames(cross_basic$covar)#make sure rownames match original cross file
+
+
+###
 
 #normalize  phenos##
 
@@ -52,10 +55,10 @@ write.csv(qtl_peaks_both, file = "qtl_peaks.csv",row.names = FALSE,quote = FALSE
 
 
 ####try while Normal####
-norm_pheno = as.data.frame(log10(cross_basic$pheno[,c(6:14,16,17,21,23:32,34,35,37:41,43:49,51,52,54:58,60:70,72,74,76)]+0.0000001))
+norm_pheno = as.data.frame(log10(cross_basic$pheno[,c(6:14,16,17,21,23:32,34,35,37:41,43:49,51,52,54:58,60:70,72,74,76)])+0.0000001)
 pheno_combined = cbind(norm_pheno, cross_basic$pheno[,c(15,18,19,20,22,33,36,42,50,53,59)])
 new_covar = covar
-new_covar[,c(3)] = log10(new_covar[,c(3)]+0.0000001)
+new_covar[,c(3)] = log10(new_covar[,c(3)])+0.0000001
 
 DO_qtl_scan_normal = scan1(apr, pheno_combined, k_loco, Xcovar=Xcovar, addcovar = new_covar[,c("sex", "age_at_sac_days","body_weight","generationG24","generationG25","generationG26","generationG27","generationG28","generationG29","generationG30","generationG31","generationG32","generationG33")],cores = 2)
 save(DO_qtl_scan_normal,file = "./results/Rdata/DO_qtl_scan_norm.Rdata")
@@ -68,9 +71,44 @@ DO_qtl_scan_binary_norm = scan1(apr, cross_basic$pheno[,c(71,73,75,77)], Xcovar=
 qtl_peaks_bin_norm = find_peaks(DO_qtl_scan_binary_norm, cross_basic$pmap, threshold=4, drop=1.5)
 qtl_peaks_both_norm = rbind(qtl_peaks_norm,qtl_peaks_bin_norm)
 
+##look at work post yield and toughness. Chr2 , goes away when normalized. Due to extreme phenotypes?
+WPY = scan1coef(apr[,2], pheno = cross_basic$pheno[,"bending_work_post_yield"],kinship = k_loco[["2"]], covar[,c("sex", "age_at_sac_days","body_weight","generationG24","generationG25","generationG26","generationG27","generationG28","generationG29","generationG30","generationG31","generationG32","generationG33")],cores = 2)
+WPY = clean_scan1(WPY)
+
+plot_coefCC(WPY, cross_basic$pmap)
+
+WPY_blup = scan1blup(apr[,2], pheno = cross_basic$pheno[,"bending_work_post_yield"],kinship = k_loco[["2"]], covar[,c("sex", "age_at_sac_days","body_weight","generationG24","generationG25","generationG26","generationG27","generationG28","generationG29","generationG30","generationG31","generationG32","generationG33")],cores = 2)
+
+WPY_blup = clean_scan1(WPY_blup)
+plot_coefCC(WPY_blup, cross_basic$pmap)
+
+
+m = maxmarg(pr,minprob = 0.5,chr = 2, pos=90.856352, map = cross_basic$pmap,return_char = TRUE)
+plot_pxg(geno = m, pheno = cross_basic$pheno[,"bending_work_post_yield"],sort = TRUE, force_labels = TRUE)
+
+x = find_marker(cross_basic$pmap,chr = 2,pos = 90.856352)
+which(pheno_wpy == sort(pheno_wpy,decreasing = T)[1])
+pr$`2`["54",,x]
+pr$`2`["341",,x]
+#####
+#WHAT DO ZERO VALUES MEAN?
+pheno_wpy = cross_basic$pheno[,"bending_work_post_yield"]
+pheno_wpy = na.omit(pheno_wpy)
+pheno_wpy = pheno_wpy[-which(pheno_wpy == sort(pheno_wpy,decreasing = T)[1])]
+WPY_q = scan1(apr[,2], pheno = pheno_wpy,kinship = k_loco[["2"]], covar[,c("sex", "age_at_sac_days","body_weight","generationG24","generationG25","generationG26","generationG27","generationG28","generationG29","generationG30","generationG31","generationG32","generationG33")],cores = 2)
+plot(WPY_q, cross_basic$pmap)
+#####
 #perms
 norm_perm = scan1perm(apr,pheno_combined,k_loco, Xcovar=Xcovar, addcovar = new_covar[,c("sex", "age_at_sac_days","body_weight","generationG24","generationG25","generationG26","generationG27","generationG28","generationG29","generationG30","generationG31","generationG32","generationG33")],n_perm = 1000,perm_Xsp = TRUE,cores = 0)
 save(norm_perm,file = "./results/Rdata/norm_perm.Rdata")
 
 perm = scan1perm(apr, cross_basic$pheno[,c(6:70,72,74,76)], k_loco, Xcovar=Xcovar, addcovar = covar[,c("sex", "age_at_sac_days","body_weight","generationG24","generationG25","generationG26","generationG27","generationG28","generationG29","generationG30","generationG31","generationG32","generationG33")],n_perm = 1000,perm_Xsp = TRUE, cores=0)
 save(perm,file = "./results/Rdata/perm.Rdata")
+
+
+#54 in the middle for slc39a13, near the highest for ptprj, near lowest for Kbtbd4, higher for Celf1, 
+#second lowest for ddb2 (lowest is 397, 3rd is 96), 
+k = calc_kinship(probs = pr)
+herit = est_herit(pheno = pheno_combined,kinship = k,addcovar = new_covar[,c("sex", "age_at_sac_days","body_weight","generationG24","generationG25","generationG26","generationG27","generationG28","generationG29","generationG30","generationG31","generationG32","generationG33")])
+
+
