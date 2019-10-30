e07708c7f808e1707e8b046bcc5bff7e609a29e5
basel-maher <bma8ne@virginia.edu>
2019-10-30 11:17:13 -0400

    new. RUN ON RIVANNA

---
+++ src/learn_bn.R
@@ -0,0 +1,33 @@
+library(igraph)
+library(bnlearn)
+########
+load("./edata.RData")
+
+load("./geneModMemAnnot_power4.RData")
+
+#BASH STUFF#
+args <- commandArgs()
+print(args)
+num = as.numeric(args[6])
+print(num)
+###
+
+resid = edata
+#module membership for all genes in all modules (from auto_wgcna.R)
+connect = geneModMemAnnot
+
+clr = unique(connect$color)
+
+color_a = clr[num]
+print(color_a)
+
+mod_genes = connect[which(connect$color==color_a),"gene"]
+mod_genes_membership = connect[which(connect$gene %in% mod_genes),c("gene","gene_id",paste0("ME",color_a),"color")]
+mod_genes_membership = mod_genes_membership[which(mod_genes_membership$color == color_a),]
+mod_genes_exp = as.data.frame(resid[,which(colnames(resid) %in% mod_genes_membership$gene)])
+
+bn = mmhc(mod_genes_exp)
+print("check")
+varName = paste0("hybrid_",color_a,"_nobl_4")
+
+save(bn, file = paste0("./bn_4/",varName,".RData"))
