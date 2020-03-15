ba4c1444e520259dc283ae0613302de6a7469a05
basel-maher <bma8ne@virginia.edu>
2020-03-15 15:13:01 -0400

    messed around, didnt really end up changing much from original

--- src/make_annot.R
+++ src/make_annot.R
@@ -1,3 +1,6 @@
+#make sure there are no factors. factors mess everything up and are a terrible invention
+options(stringsAsFactors = FALSE)
+
 #make a unified annotation file from the gene abundance files (stringtie output)
 f = list.files("./results/flat/RNA-seq/abund/")
 
@@ -11,18 +14,22 @@ for(i in f){
   print(i)
 }
 
+
 z = unique(z)
+#save(z, file="~/Desktop/z.RData")
 
 #z_trim = z[-which(z$TPM == 0),]
 z_trim=z
 #take largest transcript as main
 z_trim2=as.data.frame(matrix(ncol=ncol(z_trim), nrow=100000))
 
+#keep longest transcript
 colnames(z_trim2) = colnames(z_trim)
+z_trim2$len = NA
 i=1
 for(transc in unique(z_trim$Gene.ID)){
   x = subset(z_trim, z_trim$Gene.ID== transc)
-  x$len = abs(x$End - x$Start)
+  x$len = abs(as.numeric(x$End) - as.numeric(x$Start))
   x = x[which(x$len == max(x$len)),][1,]
   #x = x[,-6]
   z_trim2[i,] =  x
@@ -56,7 +63,8 @@ z$Gene.Name[48778] = paste0(z$Gene.Name[48778],".2")
 
 which(duplicated(z$Gene.Name))
 
-z = z[,c(1,2)]
+#z = z[,c(1,2)]
+z = z[,-z$len]
 write.csv(z,file="./results/flat/annot_file.csv", row.names = F, quote = F)
 
 
