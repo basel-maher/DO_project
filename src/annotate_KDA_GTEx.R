66f49f60d52b802a1b5a2c3ba57dfa3446b29b23
basel-maher <bma8ne@virginia.edu>
2020-03-17 16:21:51 -0400

    add impc and coloc data to each kda table

--- src/annotate_KDA_GTEx.R
+++ src/annotate_KDA_GTEx.R
@@ -92,18 +92,92 @@ coloc = coloc[which(as.numeric(coloc$H4) >=0.75),]
 coloc$gene = tolower(coloc$gene)
 
 aa = coloc[order(coloc$gene, coloc$H4,decreasing = T), ] #sort by id and reverse of abs(value)
-aa = aa[ !duplicated(aa$gene), ]  
+#aa = aa[ !duplicated(aa$gene), ]  
 ##
-#write.csv(aa,file = "./results/flat/coloc/all_coloc_greaterorover75",quote = F,row.names = F)
+write.csv(aa,file = "./results/flat/coloc/all_coloc_greaterorover75",quote = F,row.names = F)
 
 
 aa = read.csv("./results/flat/coloc/all_coloc_greaterorover75", stringsAsFactors = FALSE)
 ###
-nominal_hum[which(tolower(nominal_hum) %in% aa$gene)]
+aa_nom = aa[which(aa$gene %in% tolower(nominal_hum)),]
+#aa_ebmd = aa_nom[which(aa_nom$BMD == "BMD"),]
+#aa_FN = aa_nom[which(aa_nom$BMD == "FNBMD"),]
+#aa_LS = aa_nom[which(aa_nom$BMD == "LSBMD"),]
+
+##
+#annotate kda tables with coloc data
+kda_all = read.csv("./results/flat/key_driver_analysis_sexcombined_sft4.csv")
+kda_F = read.csv("./results/flat/key_driver_analysis_FEMALES_sft4.csv")
+kda_M = read.csv("./results/flat/key_driver_analysis_MALES_sft5.csv")
+
+#
+kda_all$eBMD = kda_F$eBMD = kda_M$eBMD = NA
+kda_all$FNBMD = kda_F$FNBMD = kda_M$FNBMD = NA
+kda_all$LSBMD = kda_F$LSBMD = kda_M$LSBMD = NA
+kda_all$PPH4 = kda_F$PPH4 = kda_M$PPH4 = NA
+
+#kda_all
+for(i in 1:nrow(kda_all)){
+  if(tolower(kda_all$gene[i]) %in% aa_nom$gene & kda_all$hyper[i]<=0.05){
+    x = subset(aa_nom, gene == tolower(kda_all$gene[i]))
+    
+    if("BMD" %in% x$BMD){kda_all$eBMD[i] = 1}
+    if("FNBMD" %in% x$BMD){kda_all$FNBMD[i] = 1}
+    if("LSBMD" %in% x$BMD){kda_all$LSBMD[i] = 1}
+    kda_all$PPH4[i] = max(x$H4)
+    
+  }
+}
+########
+#kda_F
+#
+for(i in 1:nrow(kda_F)){
+  if(tolower(kda_F$gene[i]) %in% aa_nom$gene & kda_F$hyper[i]<=0.05){
+    x = subset(aa_nom, gene == tolower(kda_F$gene[i]))
+    
+    if("BMD" %in% x$BMD){kda_F$eBMD[i] = 1}
+    if("FNBMD" %in% x$BMD){kda_F$FNBMD[i] = 1}
+    if("LSBMD" %in% x$BMD){kda_F$LSBMD[i] = 1}
+    kda_F$PPH4[i] = max(x$H4)
+    
+  }
+}
+
+
+########
+#kda_M
+#
+for(i in 1:nrow(kda_M)){
+  if(tolower(kda_M$gene[i]) %in% aa_nom$gene & kda_M$hyper[i]<=0.05){
+    x = subset(aa_nom, gene == tolower(kda_M$gene[i]))
+    
+    if("BMD" %in% x$BMD){kda_M$eBMD[i] = 1}
+    if("FNBMD" %in% x$BMD){kda_M$FNBMD[i] = 1}
+    if("LSBMD" %in% x$BMD){kda_M$LSBMD[i] = 1}
+    kda_M$PPH4[i] = max(x$H4)
+    
+  }
+}
 
 
 
 
 
+
+#####ADD IMPC
+kda_all$impc = kda_F$impc = kda_M$impc = NA
+
+
+impc = read.csv("./results/flat/IMPC_BMD_Results.csv")
+impc_gene = impc[which(impc$genotype_p_value<=0.05),"Gene"]
+
+kda_all[which(tolower(kda_all$gene) %in% impc_gene),"impc"] = 1
+kda_F[which(tolower(kda_F$gene) %in% impc_gene),"impc"] = 1
+kda_M[which(tolower(kda_M$gene) %in% impc_gene),"impc"] = 1
+
+write.csv(kda_all, file = "~/Desktop/kda_combined.csv", quote = F, row.names = F)
+write.csv(kda_F, file = "~/Desktop/kda_F.csv", quote = F, row.names = F)
+
+write.csv(kda_M, file = "~/Desktop/kda_M.csv", quote = F, row.names = F)
 
 
