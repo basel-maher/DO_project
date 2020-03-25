5bcf6456dda41e5efbab6ff35875fd4aac4e64d8
basel-maher <bma8ne@virginia.edu>
2020-03-24 21:54:44 -0400

    plots

--- src/plots_mostRecent.R
+++ src/plots_mostRecent.R
@@ -11,6 +11,8 @@ library(ggrepel)
 library(ggplotify)
 library(tidyr)
 library(reshape2)
+library(emmeans)
+library(ggsignif)
 
 #
 #load the geno probs
@@ -618,4 +620,111 @@ ggplot(data = dat2.melt, aes(x=Genotype, y=Activity,Group=Mutation,fill=Mutation
   ylab('QSOX1 Activity (pmol H2O2/min/ul')
 
 
+####4C
+# generate LSMEANS by sex adjusting for weight, length and mutation type
+# can add mutation type to the filter statement to look at effects
+# of individual mutation
+dat1<-read.csv('./data/pheno_data/Qsox1_data/qsox1_caliper.csv',header=T,na.strings='NA')
 
+dat1$Genotype<-factor(dat1$Genotype, levels=c('wt','Het','Mut'))
+dat1<-filter(dat1,dat1$Mom_genotype!='B6' & dat1$Dad_genotype!='B6')
+dat1$ML_all<-(dat1$right_Femur_ML+dat1$left_Femur_ML)/2
+
+
+dat1.m<-filter(dat1,dat1$Sex=='M')
+
+
+lf.lm.12<-lm(ML_all~Genotype+Weight+Qsox_Mutation+Length,data=dat1.m)
+anova(lf.lm.12)
+lf<-lsmeans(lf.lm.12,"Genotype")
+par(mfrow=c(2,2))
+plot(lf,horizontal=F,ylab='Femur Length (mm)')
+lff = lsmeans(lf.lm.12,pairwise~Genotype, adj='tukey')
+
+
+lff = as.data.frame(lff)
+ggplot(lff,aes(x=lsmeans.Genotype)) + geom_errorbar(data = lff, aes(ymin=lsmeans.lower.CL, ymax=lsmeans.upper.CL),color="blue",size=3.5,width=0.15) +
+  theme(axis.text = element_text(size=18), axis.title = element_text(size=18))+geom_point(aes(y=lsmeans.lsmean),size=5)+geom_signif(comparisons = list(c("wt", "Het"),c("wt","Mut"),c("Het","Mut")), annotations=c("2.4e-3","2.87e-7","0.044"),aes(y=lsmeans.lsmean),y_position = c(1.95,1.98,1.97)) + ylim(1.83,1.98)
+
+
+#females
+dat1.f<-filter(dat1,dat1$Sex=='F')
+
+
+lf.lm.12<-lm(ML_all~Genotype+Weight+Qsox_Mutation+Length,data=dat1.f)
+anova(lf.lm.12)
+lf<-lsmeans(lf.lm.12,"Genotype")
+par(mfrow=c(2,2))
+plot(lf,horizontal=F,ylab='Femur Length (mm)')
+lff = lsmeans(lf.lm.12,pairwise~Genotype, adj='tukey')
+
+
+lff = as.data.frame(lff)
+ggplot(lff,aes(x=lsmeans.Genotype)) + geom_errorbar(data = lff, aes(ymin=lsmeans.lower.CL, ymax=lsmeans.upper.CL),color="blue",size=3.5,width=0.15) +
+  theme(axis.text = element_text(size=18), axis.title = element_text(size=18))+geom_point(aes(y=lsmeans.lsmean),size=5)+geom_signif(comparisons = list(c("wt", "Het"),c("wt","Mut"),c("Het","Mut")), annotations=c("0.8","0.004","0.021"),aes(y=lsmeans.lsmean),y_position = c(1.70,1.74,1.73)) + ylim(1.64,1.74)
+######
+#ADD SAMPLE COUNTS PER GENOTYPE
+######
+#4D
+#
+#"uCT_Ct.TMD", "uCT_Tt.Ar","ML", "uCT_Ma.Ar","uCT_Ct.porosity"
+dat1<-read.csv('./data/pheno_data/Qsox1_data/qsox_uCT.csv',header=T,na.strings='NA')
+
+dat1$Genotype<-factor(dat1$Genotype, levels=c('wt','Het','Mut'))
+
+lf.lm.12<-lm(pMOI.mm4.~Genotype+Weight..g.+Qsox.Mutation,data=dat1)
+anova(lf.lm.12)
+lf<-lsmeans(lf.lm.12,"Genotype")
+par(mfrow=c(2,2))
+plot(lf,horizontal=F,ylab=expression(paste("pMOI (",mm^4,")")))
+lff = lsmeans(lf.lm.12,pairwise~Genotype, adj='tukey')
+
+lff = as.data.frame(lff)
+ggplot(lff,aes(x=lsmeans.Genotype)) + geom_errorbar(data = lff, aes(ymin=lsmeans.lower.CL, ymax=lsmeans.upper.CL),color="blue",size=3.5,width=0.15) +
+  theme(axis.text = element_text(size=18), axis.title = element_text(size=18))+geom_point(aes(y=lsmeans.lsmean),size=5)+geom_signif(comparisons = list(c("wt", "Mut")), annotations=c("0.02"),aes(y=lsmeans.lsmean),y_position = c(0.52)) + ylim(0.36,0.52)
+
+#
+####
+####
+#4E
+lf.lm.12<-lm(Imax.mm4.~Genotype+Weight..g.+Qsox.Mutation,data=dat1)
+anova(lf.lm.12)
+lf<-lsmeans(lf.lm.12,"Genotype")
+par(mfrow=c(2,2))
+plot(lf,horizontal=F,ylab=expression(paste("Imax (",mm^4,")")))
+lff = lsmeans(lf.lm.12,pairwise~Genotype, adj='tukey')
+
+lff = as.data.frame(lff)
+ggplot(lff,aes(x=lsmeans.Genotype)) + geom_errorbar(data = lff, aes(ymin=lsmeans.lower.CL, ymax=lsmeans.upper.CL),color="blue",size=3.5,width=0.15) +
+  theme(axis.text = element_text(size=18), axis.title = element_text(size=18))+geom_point(aes(y=lsmeans.lsmean),size=5)+geom_signif(comparisons = list(c("wt", "Mut")), annotations=c("0.009"),aes(y=lsmeans.lsmean),y_position = c(0.36)) + ylim(0.25,0.36)
+
+#
+###
+####
+#4F
+lf.lm.12<-lm(Ct.Ar.Tt.Ar....~Genotype+Weight..g.+Qsox.Mutation,data=dat1)
+anova(lf.lm.12)
+lf<-lsmeans(lf.lm.12,"Genotype")
+par(mfrow=c(2,2))
+plot(lf,horizontal=F,ylab=expression(paste("Ct.Ar.Tt.Ar.... (",mm^4,")")))
+lff = lsmeans(lf.lm.12,pairwise~Genotype, adj='tukey')
+
+lff = as.data.frame(lff)
+ggplot(lff,aes(x=lsmeans.Genotype)) + geom_errorbar(data = lff, aes(ymin=lsmeans.lower.CL, ymax=lsmeans.upper.CL),color="blue",size=3.5,width=0.15) +
+  theme(axis.text = element_text(size=18), axis.title = element_text(size=18))+geom_point(aes(y=lsmeans.lsmean),size=5)+geom_signif(comparisons = list(c("wt", "Mut")), annotations=c("0.031"),aes(y=lsmeans.lsmean),y_position = c(50)) + ylim(43,50)
+
+###
+###
+###
+#4G
+#
+lf.lm.12<-lm(Tt.Ar.mm2.~Genotype+Weight..g.+Qsox.Mutation,data=dat1)
+anova(lf.lm.12)
+lf<-lsmeans(lf.lm.12,"Genotype")
+par(mfrow=c(2,2))
+plot(lf,horizontal=F,ylab=expression(paste("ttar (",mm^4,")")))
+lff = lsmeans(lf.lm.12,pairwise~Genotype, adj='tukey')
+
+lff = as.data.frame(lff)
+ggplot(lff,aes(x=lsmeans.Genotype)) + geom_errorbar(data = lff, aes(ymin=lsmeans.lower.CL, ymax=lsmeans.upper.CL),color="blue",size=3.5,width=0.15) +
+  theme(axis.text = element_text(size=18), axis.title = element_text(size=18))+geom_point(aes(y=lsmeans.lsmean),size=5)+geom_signif(comparisons = list(c("wt", "Mut")), annotations=c("0.083"),aes(y=lsmeans.lsmean),y_position = c(2.075)) + ylim(1.8,2.1)
