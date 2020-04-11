25591d7f8adfc4bb535b6a3e31fb889878e3c2dc
basel-maher <bma8ne@virginia.edu>
2020-04-11 15:08:02 -0400

    fggh

--- src/B6_OBs_RNA_Seq.R
+++ src/B6_OBs_RNA_Seq.R
@@ -34,11 +34,11 @@ gene.ann<-read.csv('./data/GSE54461_OB_RNA_SEQ_RAW/mart_export.txt',header=T)
 gene.ann<-gene.ann[,-2]
 head(gene.ann);dim(gene.ann)
 gene.ann<-subset(gene.ann, !duplicated(gene.ann[,1]))
-genes<-c('Bicc1')
+#genes<-c('Bicc1')
   # select gene or genes
-grep('ENSMUST00000182158',gene.ann[,1])
-grep('A430106G13Rik',gene.ann[,2])
-dat1<-c()
+#grep('ENSMUST00000182158',gene.ann[,1])
+#grep('A430106G13Rik',gene.ann[,2])
+#dat1<-c()
 genes<-c('Qsox1')#A930004D18Rik
 genes<-unique(genes)
 rm(dat1)
@@ -84,6 +84,7 @@ temp4$newGene<-mapvalues(temp4$Gene, from=unique(temp4$Gene), to=toupper(genes))
 
 save(temp4,file = "./results/Rdata/rasd1_calvarial.Rdata")
 
+cairo_pdf(filename="~/Desktop/figs/6A.pdf", width = 10, height = 7)
 ggplot(temp4, aes(x=Day, y=mean,ymin=mean-temp4$sem, ymax=mean+temp4$sem, colour=newGene,Group=newGene)) + 
   geom_errorbar(aes(ymin=mean-temp4$sem, ymax=mean+temp4$sem), width=0.5, size=1.25) +
   geom_line(aes(group=newGene), size=0.5) +
@@ -91,4 +92,4 @@ ggplot(temp4, aes(x=Day, y=mean,ymin=mean-temp4$sem, ymax=mean+temp4$sem, colour
                                       breaks=as.character(unique(temp4$Gene)),
                                       labels=genes,l=60) +
   scale_y_continuous() + facet_wrap(~newGene, scales='free_y') 
-
+dev.off()
